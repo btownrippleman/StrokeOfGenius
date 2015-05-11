f563b6e1a086c19d85f84ff2c78a6494c5b092bc
root <bradenripple@gmail.com>
2015-05-11 10:54:09 -0700

    got a correlation between circle size and color

--- main.lua
+++ main.lua
@@ -11,42 +11,49 @@ bg:setFillColor(0,0,0)
 
 
 function fadingCircle(x,y,rad,strokeWidth,color)
- local a,b,c = 0,1,0;
+ local colors = {1,0,1}
   local    colors = color --{color[1]-a*math.random(),color[3]-b*math.random(),color[3]-c*math.random()}
         local xval = x+strokeWidth*math.random()
         local yval = y+strokeWidth*math.random()
         local radius = rad * math.random()*math.random()*math.random()
 
-  local circle = display.newCircle(xval,yval,radius)
+        local circle = display.newCircle(xval,yval,radius)
+        -- circle.alpha =  radius/rad
+        colors[2] = (1-radius/rad)*(1-radius/rad)*(1-radius/rad)
         circle:setFillColor(unpack(colors))
-        transition.to(circle,{time = 1000, alpha = 0})
-
-        if circle.alpha == 0 then circle:removeSelf() end
+        local options =
+        {
+          time = colors[2]*6*1500*math.random(),
+         alpha = 0,
+         onComplete = remove,
+         easing= outInExpo
+         }
+        transition.to(circle,options)
 
 end
 
+function remove(obj)
+  obj:removeSelf()
+end
 
+function randomFunction(ev)
+
+end
 
 function touched(event)
 
   local color = {1,0,1}
-  local radius = 7
-  local strokeWidth = 20
+  local radius = 25
+  local strokeWidth = 35
 
   if event.phase == "moved" then
-    for i = 1,4 do
-      fadingCircle(event.x,event.y,radius,strokeWidth,color) end
+    for i = 1,11 do
+      fadingCircle(event.x,event.y,radius,strokeWidth,color)
+      if math.random() < .002 then randomFunction(event) end
     end
 
 end
-
-
-bg:addEventListener("touch",touched)
-
-
-
-
-
+end
 
 
 bg:addEventListener("touch",touched)
